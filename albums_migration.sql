c30ece2596fc54b84901a2b1ca8072280d58166b
Chris Davis <christopher.j.davis03@gmail.com>
2022-07-06 12:12:09 -0500

    albums done

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,11 +1,11 @@
 USE codeup_test_db;
-DROP DATABASE IF EXISTS albums;
-CREATE DATABASE albums(
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(50),
     name VARCHAR(50) NOT NULL,
     release_date INT UNSIGNED,
-    sales DECIMAL (4,2)
-    genre VARCHAR(20)
+    sales DECIMAL (4,2),
+    genre VARCHAR(20),
     PRIMARY KEY (id)
 );
